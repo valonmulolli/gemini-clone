94966b4848678690117319da4d0b12e71937c20b
valonmulolli <valonmulolli@gmail.com>
2024-03-24 20:18:27 +0100

    Improve ContextProvider with state management and chat handling

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,25 +1,84 @@
-import { createContext } from "react";
-import runChat from "../config/config";
+import { createContext } from 'react';
+import runChat from '../config/config';
+import { useState } from 'react';
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-  
-  const onSent = async (prompt) => {
-    runChat(prompt);
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const delayParams = (index, nextWord) => {
+    setTimeout(function() {
+      setResultData((prev) => prev + nextWord);
+    }, 10 * index);
+  };
+  const newChat = () => {
+    setLoading(false);
+    setShowResults(false)
   }
 
-  onSent("what is Typescript")
-  
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResults(true);
+    let response;
+    if (prompt !== undefined) {
+      response = await runChat(prompt);
+      setRecentPrompt(prompt)
+    } else {
+      setPrevPrompts(prev => [...prev, input]);
+      setRecentPrompt(input);
+      response = await runChat(input);
+    }
+
+    try {
+
+
+      let responseArray = response.split("**");
+      let newResponse = "";
+      for (let i = 0; i < responseArray.length; i++) {
+        if (i === 0 || i % 2 !== 1) {
+          newResponse += responseArray[i];
+        } else {
+          newResponse += "<b>" + responseArray[i] + "</b>";
+        }
+      }
+      let newResponse2 = newResponse.split("*").join("<br/>");
+      let newResponseArray = newResponse2.split("");
+      for (let i = 0; i < newResponseArray.length; i++) {
+        const nextWord = newResponseArray[i];
+        delayParams(i, nextWord + "");
+      }
+    } catch (error) {
+      console.error("Error while running chat:", error);
+      // Handle error appropriately
+    } finally {
+      setLoading(false);
+      setInput("");
+    }
+  };
+
   const contextValue = {
-    
-  }
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResults,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
 
   return (
-    <Context.Provider value={contextValue}>
-      {props.children}
-    </Context.Provider>
-  )
-}
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
 
-export default ContextProvider
+export default ContextProvider;
